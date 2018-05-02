e631be30cb90e71d064903e52bf333625ab89ef8
Jesús Cueto <DMSoftware@github.com>
2018-05-01 21:26:52 -0500

    Complete provisional interface

---
+++ app/src/main/java/com/dmsoftware/followlife/fragments/AppointmentDetailFragment.java
@@ -0,0 +1,30 @@
+package com.dmsoftware.followlife.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dmsoftware.followlife.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppointmentDetailFragment extends Fragment {
+
+
+    public AppointmentDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_appointment_detail, container, false);
+    }
+
+}
